12c8d0c6a9370f67da1e940aac435b98c2ac06fd
abishekvarma <abishek@entrolabs.com>
2026-01-21 12:31:06 +0530

    backend folder structure and db integration

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import app from "./app.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+// // Connect DB
+// connectDB();
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
